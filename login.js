0e75ef52bca5bff08ca22f06ca2394e063814c49
Xhuri <111800311+IAlphaOmegaI@users.noreply.github.com>
2022-12-07 23:32:52 +0100

    kkk

--- login.js
+++ login.js
@@ -1,42 +1,56 @@
 //LOG IN---------------------------------------------------------------------------------------------------
+
 function Checking() {
   var passwordInput = $(".passwordInput").val();
   var emailInput = $(".emailInput").val();
-  // if (emailInput == "" && (passwordInput = "")) {
-  //   alert("Both Email and Password are missing");
-  // } else if (emailInput == "") {
-  //   alert("Email missing");
-  // } else if (passwordInput == "") {
-  //   alert("Passwod missing");
-  //EMAIL
-  //PASSWORD
-  var user =
-    '{ "email": ["Xhurian@gmail.com","flavio@outlook.com","Luis@yahoo.com"], "password": ["Qafshtama1.","Lajthiza2.","Selita3."]}';
-  var json = JSON.parse(user);
-  // else if ((json.password.indexOf(passwordInput) > -1)&&(json.email.indexOf(emailInput)>-1)) {
-  //   alert("Correct");
-  // }
-  var Epattern = new RegExp("^(?=.*[@.]).+$");
-  var pattern = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*.,?]).+$");
-  if (Epattern.test(emailInput)) {
-    console.log("Email is " + emailInput);
-    // alert("Email is " + emailInput);
-  } else if (!Epattern.test(emailInput)) {
-    alert("Email must include @ and . character");
-  }
-  if (pattern.test(passwordInput) && passwordInput.length > 7) {
-    console.log("Password is " + passwordInput);
-    // alert("Password is " + passwordInput);
-  } else if (!Epattern.test(passwordInput)) {
-    alert(
-      "Password must include at least one Uppercase letter, one Lowercase letter and a special character. It also should be at least 8 characters long."
+  if (emailInput == "" && (passwordInput = "")) {
+    alert("Both Email and Password are missing");
+    return Checking;
+  } else if (emailInput == "") {
+    alert("Email missing");
+    return Checking;
+  } else if (passwordInput == "") {
+    alert("Password missing");
+    return Checking;
+  } else {
+    //EMAIL
+    //PASSWORD
+    var user =
+      '{ "email": ["Xhurian@gmail.com","flavio@outlook.com","Luis@yahoo.com"], "password": ["Qafshtama1.","Lajthiza2.","Selita3."]}';
+    var json = JSON.parse(user);
+    // else if ((json.password.indexOf(passwordInput) > -1)&&(json.email.indexOf(emailInput)>-1)) {
+    //   alert("Correct");
+    // }
+    var Epattern = new RegExp("^(?=.*[@.]).+$");
+    var pattern = new RegExp(
+      "^(?=.*[a-z])(?=.*[A-Z])(?=.*[-+_!@#$%^&*.,?]).+$"
     );
-  } else if (Epattern.test(emailInput) && Epattern.test(passwordInput)) {
-    if (json.email.indexOf(emailInput)) {
-      alert("Your user isnt registred");
+    let nr = 0;
+    if (Epattern.test(emailInput)) {
+      console.log("Email is " + emailInput);
+      // alert("Email is " + emailInput);
+      nr++;
+    } else if (!Epattern.test(emailInput)) {
+      alert("Email must include @ and . character");
     }
-    if (json.password.indexOf(passwordInput)) {
-      alert("Incorrect Password");
+    if (pattern.test(passwordInput) && passwordInput.length > 7) {
+      console.log("Password is " + passwordInput);
+      // alert("Password is " + passwordInput);
+      nr++;
+    } else if (!Epattern.test(passwordInput)) {
+      alert(
+        "Password must include at least one Uppercase letter, one Lowercase letter and a special character. It also should be at least 8 characters long."
+      );
+    }
+    // if (Epattern.test(emailInput) && Epattern.test(passwordInput))
+    if ((nr = 2)) {
+      if (json.email.indexOf(emailInput)) {
+        alert("Your user isnt registred");
+        return Checking;
+      }
+      if (json.password.indexOf(passwordInput)) {
+        alert("Incorrect Password");
+      }
     }
   }
 }
